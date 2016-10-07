1f1f09acf9ddee7fdd6c675d53b93ca5259c3243
Mark Griffiths <mark@thebespokepixel.com>
2016-10-07 13:33:25 +0100

    Update docs

--- gulpfile.js
+++ gulpfile.js
@@ -9,19 +9,31 @@ const cordial = require('@thebespokepixel/cordial')()
 
 // transpilation/formatting
 gulp.task('bundle', cordial.macro({
-	master: false,
+	source: 'src/lib/index.js'
+}).bundle())
+
+gulp.task('master', cordial.macro({
+	master: true,
 	source: 'src/lib/index.js'
 }).bundle())
 
 // Clean
 gulp.task('clean', cordial.shell({
-	source: ['npm-debug.log', './nyc_output', './test/coverage']
+	source: ['npm-debug.log', './nyc_output', './coverage']
 }).trash())
+
+// Docs
+gulp.task('docs', cordial.shell({
+	source: 'npm run doc-build'
+}).job())
 
 // Tests
 gulp.task('ava', cordial.test().ava(['test/*.js']))
 gulp.task('xo', cordial.test().xo(['src/lib/*.js']))
 gulp.task('test', gulp.parallel('xo', 'ava'))
 
+// Hooks
+gulp.task('start-release', gulp.series('reset', 'clean', 'docs', 'master'))
+
 // Default
-gulp.task('default', gulp.series('bump', 'bundle'))
+gulp.task('default', gulp.series('bump', 'clean', 'docs', 'bundle'))
