acb0694672df129f2d51b8b6d1e0b9f925934501
Mark Griffiths <mark@thebespokepixel.com>
2021-10-30 10:24:48 +0100

    Update Documentation

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
 import { readPackageUpSync } from 'read-pkg-up';
 
-/* ──────────────────────╮
- │ @thebespokepixel/meta │
- ╰───────────────────────┴───────────────────────────────────────────────────── */
-
-/**
- * Creates the metadata collection function, starting at the path provided or
- * the current working directory by default.
- * @function meta
- * @param  {String} cwd The directory to start searching for a package.json file.
- * @return {metadata}   The map of reduced package metadata.
- */
 function meta(cwd = '.') {
 	const pkg = readPackageUpSync({cwd}).packageJson;
-
-	/**
-	 * Extract metadata for sharing inside a package.
-	 * @const {metadata}
-	 * @property {String} name          The package's name
-	 * @property {String} bin           The CLI binary we provide
-	 * @property {String} description   The description from package.json
-	 * @property {String} license       The package license
-	 * @property {String} bugs          Our issues queue
-	 */
 	const metadata = {
 		get name() {
 			return pkg.name
@@ -34,7 +13,6 @@ function meta(cwd = '.') {
 			if (pkg.copyright && pkg.copyright.year) {
 				return `©${pkg.copyright.year} ${pkg.copyright.owner}`
 			}
-
 			return pkg.copyright ? pkg.copyright :
 				`©${new Date().getFullYear()} ${pkg.author.name}`
 		},
@@ -47,25 +25,13 @@ function meta(cwd = '.') {
 		get bin() {
 			return pkg.bin ? Object.keys(pkg.bin)[0] : 'none'
 		},
-		/**
-		 * Print a package version string.
-		 * @param  {Number} style The version string format wanted:
-		 * ```
-		 * 1: Simple number format: 0.1.2
-		 * 2: Long version with name: @thebespokepixel/meta v0.1.2
-		 * 3: v-prefixed version number: v0.1.2
-		 * ```
-		 * @return {String} The version string.
-		 */
 		version: (style = 1) => {
 			const version = (function () {
 				if (pkg.buildNumber > 0) {
 					return `${pkg.version}-Δ${pkg.buildNumber}`
 				}
-
 				return `${pkg.version}`
 			})();
-
 			switch (style) {
 				case 4:
 					return `${pkg.version}`
@@ -78,7 +44,6 @@ function meta(cwd = '.') {
 			}
 		}
 	};
-
 	return metadata
 }
 
