9ac6716ebe4a48da3035aeedff9c00b53ed33101
Mark Griffiths <mark@thebespokepixel.com>
2021-10-30 10:17:48 +0100

    Add clean up

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import cleanup from 'rollup-plugin-cleanup'
 
 const external = id => !id.startsWith('src') && !id.startsWith('.') && !id.startsWith('/') && !id.startsWith('\0')
 
 const config = [{
 	external,
-	plugins: [resolve(), commonjs()],
+	plugins: [resolve(), commonjs(), cleanup()],
 	input: 'src/index.js',
 	output: {
 		file: 'index.js',
